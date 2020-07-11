1e012adb2f948a63f3da1400e5ab1d0040f7618d
KOREAparksh <45682868+KOREAparksh@users.noreply.github.com>
2020-07-11 23:48:55 +0900

    [add]add stationList, layout

---
+++ app/src/main/java/com/outsource/subwaypeopledensity/model/SubwayStation.java
@@ -0,0 +1,32 @@
+package com.outsource.subwaypeopledensity.model;
+
+import java.util.ArrayList;
+
+public class SubwayStation {
+
+    public static final String[] daejeonStations = {
+            "반석",
+            "지족",
+            "노은",
+            "월드컵경기장",
+            "현충원",
+            "구암",
+            "유성온천",
+            "갑천",
+            "월평",
+            "갈마",
+            "정부청사",
+            "시청",
+            "탄방",
+            "용문",
+            "오룡",
+            "서대전네거리",
+            "중구청",
+            "중앙로",
+            "대전역",
+            "대동",
+            "신흥",
+            "판암"
+    };
+
+}
